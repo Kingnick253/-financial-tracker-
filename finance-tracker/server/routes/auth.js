3a8118a98ef8243f6cd1fa27c1f031192776e020
Nic <nicholaswebb2016@gmail.com>
2025-05-11 23:01:06 -0700

    finished route for login

--- finance-tracker/server/routes/auth.js
+++ finance-tracker/server/routes/auth.js
@@ -19,3 +19,27 @@ router.post('/signup', async (req, res ) => {
     }
 
 });
+
+router.post ('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body; // Destructure email and password from request body
+        const user = await User.findOne({ email }); // Find user by email in the database
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email' }); // Respond with error if user not found
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password); // Compare provided password with hashed password in database
+
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid password' }); // Respond with error if passwords do not match
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' }); // Generate JWT token
+        res.status(200).json({ token }); // Respond with the generated token
+    } catch (error) {
+        res.status(500).json({ message: 'Error Failed to login' }); // Respond with error message
+    }
+});
+
+module.exports = router; // Export the router for use in other parts of the application
